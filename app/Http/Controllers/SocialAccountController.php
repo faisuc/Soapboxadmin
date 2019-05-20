afde082a22c7d804fed83ac57c5fefba3af74dc8
kunal <kunalsoni3331@gmail.com>
2019-05-20 17:54:47 +0530

    Add Facebook library to controller

--- app/Http/Controllers/SocialAccountController.php
+++ app/Http/Controllers/SocialAccountController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Sentinel;
+use Session;
+use Redirect;
+use URL;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
 
 class SocialAccountController extends Controller
 {
