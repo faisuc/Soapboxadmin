e994339e158727d0b529871277ba00463d83a734
kunal <kunalsoni3331@gmail.com>
2019-05-20 18:17:09 +0530

    display login url

--- app/Http/Controllers/SocialAccountController.php
+++ app/Http/Controllers/SocialAccountController.php
@@ -49,7 +49,7 @@ class SocialAccountController extends Controller
     {
         $this->setFacebookObject();
         // Session::flush();
-        // echo 'Token: '.Session::get('fb_access_token'); die();
+        echo 'Token: '.Session::get('fb_access_token').'<br>';
         if(Session::get('fb_access_token') == '')
         {
             $helper = $this->api->getRedirectLoginHelper();
