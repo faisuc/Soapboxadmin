fb11c03528499e504c3a0b7edb62843c89d1f6d2
kunal <kunalsoni3331@gmail.com>
2019-05-20 18:10:09 +0530

    session unset

--- app/Http/Controllers/SocialAccountController.php
+++ app/Http/Controllers/SocialAccountController.php
@@ -48,6 +48,7 @@ class SocialAccountController extends Controller
     public function store(Request $request)
     {
         $this->setFacebookObject();
+        Session::forget('fb_access_token'); die();
         if(Session::get('fb_access_token') == '')
         {
             $helper = $this->api->getRedirectLoginHelper();
