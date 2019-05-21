274d81cea4e2c40bfbd7c820a7f58350fad92c77
kunal <kunalsoni3331@gmail.com>
2019-05-21 12:06:29 +0530

    Remove session

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -321,7 +321,7 @@ class PostController extends Controller
     public function fb_publish_post($post_id = null)
     {
         $this->setFacebookObject();
-        Session::flush();
+        // Session::flush();
         // echo 'Token: '.Session::get('fb_access_token'); die();
         if(Session::get('fb_access_token') == '')
         {
@@ -332,8 +332,8 @@ class PostController extends Controller
             return redirect()->away($loginUrl);
             echo "Not Redirecting. Error Occur"; die();
         }
-        
-        echo 'Token: '.Session::get('fb_access_token'); die();
+
+        echo 'Token 2: '.Session::get('fb_access_token').'<br>';
         $token = Session::get('fb_access_token');
 
         /**/
