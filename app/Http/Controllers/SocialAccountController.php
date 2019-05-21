1fea12d57420f237bf9b8e205d57357df0b5086e
kunal <kunalsoni3331@gmail.com>
2019-05-21 13:29:07 +0530

    Redirect issue fix

--- app/Http/Controllers/SocialAccountController.php
+++ app/Http/Controllers/SocialAccountController.php
@@ -108,7 +108,7 @@ class SocialAccountController extends Controller
             $helper = $this->api->getRedirectLoginHelper();
             $permissions = ['email','user_posts','manage_pages','publish_pages'];
             $loginUrl = $helper->getLoginUrl(URL::to('/').'/fb_callback', $permissions);
-            // echo $loginUrl;
+            echo $loginUrl; die();
             return redirect()->away($loginUrl);
             echo "Not Redirecting. Error Occur"; die();
         }
