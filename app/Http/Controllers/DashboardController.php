5fc92b5d58da309d5ad6267e65bf9def52637e15
kunal <kunalsoni3331@gmail.com>
2019-07-24 16:00:28 +0530

    Twitter Fan Card in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -47,7 +47,7 @@ class DashboardController extends Controller
 	        	$oauth_token = $twitter_account->twitter_session;
 	            $oauth_token_secret = $twitter_account->twitter_secret;
 
-	            $url = 'https://api.twitter.com/1.1/statuses/show.json?screen_name=KunalSo98628814';
+	            $url = 'https://api.twitter.com/1.1/users/show.json?screen_name=KunalSo98628814';
 	            // $parameters = array('screen_name' => 'KunalSo98628814');
 	            $parameters = array();
 	            $result = $this->Request($url, 'get', $consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret, $parameters);
