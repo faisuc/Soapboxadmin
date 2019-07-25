6ea78af6917b1ad3cc10749d497dea600c4e4a30
Faisan <necafasu@gmail.com>
2019-07-25 21:36:00 +0800

    Update footer soapbox to socialhat

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                    Copyright © 2018 Soapbox. All rights reserved.
+                    Copyright © 2018 Social Hat. All rights reserved.
             </div>
             <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
                 <div class="text-md-right footer-links d-none d-sm-block">
