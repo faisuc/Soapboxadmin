45912907ad4804ff13b8d8b4301fa08b947f7a00
kunal <kunalsoni3331@gmail.com>
2019-11-29 12:28:07 +0530

    post save date change issue fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -834,12 +834,12 @@ class PostController extends Controller
 
         $post->link = $link;
         $post->schedule_to_post_date = Carbon::createFromFormat('Y-m-d H:i A', $schedule_date)->toDateTimeString();
+        $post->save();
 
         echo "<pre>";
         print_r($post);
         die();
 
-        $post->save();
 
         if ($media_id != 0)
         {
