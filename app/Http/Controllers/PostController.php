da3d990e2cb126c5b24ce753269dada41d2de3a5
kunal <kunalsoni3331@gmail.com>
2019-12-03 17:15:23 +0530

    pinterest api for post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -751,6 +751,17 @@ class PostController extends Controller
             }
             if (!empty($pinterest_account)) {
                 if($pinterest_account->pinterest_token) {
+
+                    $app_id = getenv('PINTEREST_CLIENT_ID');
+                    $app_secret = getenv('PINTEREST_CLIENT_SECRET');
+                    // $callback_url = getenv('PINTEREST_REDIRECT').'?social_id='.$social_id;
+                    $callback_url = getenv('PINTEREST_REDIRECT');
+                    $pinterest = new Pinterest($app_id, $app_secret);
+                    $state = "social_id=".$social_id;
+                    $pinterest->auth->setState($state);
+                    $pinterest_url = $pinterest->auth->getLoginUrl($callback_url, array('read_public', 'write_public'));
+                    echo $pinterest_url; die();
+
                     $token = $pinterest_account->pinterest_token;
 
                     $app_id = getenv('PINTEREST_CLIENT_ID');
