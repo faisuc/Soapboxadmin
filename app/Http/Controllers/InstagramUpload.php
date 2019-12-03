13ae1524b0a397358c7c8837168de27dbe7b44d8
kunal <kunalsoni3331@gmail.com>
2019-12-03 12:21:58 +0530

    instagram api for post

--- app/Http/Controllers/InstagramUpload.php
+++ app/Http/Controllers/InstagramUpload.php
@@ -23,7 +23,7 @@ class InstagramUpload
     private $headers = array();
     // private $user_agent = "Instagram 10.3.2 Android (18/4.3; 320dpi; 720x1280; Huawei; HWEVA; EVA-L19; qcom; en_US)";
     // private $user_agent = "Mozilla/5.0 (Linux; U; Android 4.0; en-us; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30' : 'Mozilla/5.0(Windows;U;WindowsNT5.0;en-US;rv:1.4)Gecko/20030624Netscape/7.1(ax)";
-    private $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36";
+    private $user_agent = $_SERVER['HTTP_USER_AGENT'];
     public function __construct()
     {
         $this->guid      = $this->generateUUID();
@@ -32,9 +32,6 @@ class InstagramUpload
         $this->upload_id = $this->generateUploadId();
         $this->headers[] = "X-IG-Capabilities: " . $this->x_ig_capabilities;
         $this->headers[] = "X-IG-Connection-Type: WIFI";
-        echo "<pre>";
-        print_r($_SERVER);
-        die();
         $this->proxy = $_SERVER['SERVER_ADDR'];
     }
     public function Login($username = "", $password = "")
@@ -131,7 +128,7 @@ class InstagramUpload
         curl_setopt($ch, CURLOPT_URL, $strUrl);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_PROXY,$proxy);
+        curl_setopt($ch, CURLOPT_PROXY,$this->proxy);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
         curl_setopt($ch, CURLOPT_USERAGENT, $this->user_agent);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $arrPostData);
