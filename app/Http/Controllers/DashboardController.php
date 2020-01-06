c8aa6321d443a2f732466c2cf27ae2b99c65c7fb
kunal <kunalsoni3331@gmail.com>
2020-01-06 18:43:47 +0530

    test instagram api

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -523,4 +523,89 @@ class DashboardController extends Controller
         }
     }
 
+    public function callback_instagram() {
+
+        $app_id = getenv('FACEBOOK_CLIENT_ID');
+        $this->setFacebookObject();
+
+        $helper = $this->api->getRedirectLoginHelper();
+        if (isset($_GET['state'])) {
+            $helper->getPersistentDataHandler()->set('state', $_GET['state']);
+        }
+        try {
+            $accessToken = $helper->getAccessToken();
+        }
+        catch(Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        }
+        catch(Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+
+        if (! isset($accessToken)) {
+            if ($helper->getError()) {
+                header('HTTP/1.0 401 Unauthorized');
+                echo "Error: " . $helper->getError() . "\n";
+                echo "Error Code: " . $helper->getErrorCode() . "\n";
+                echo "Error Reason: " . $helper->getErrorReason() . "\n";
+                echo "Error Description: " . $helper->getErrorDescription() . "\n";
+            }
+            else {
+                header('HTTP/1.0 400 Bad Request');
+                echo 'Bad request';
+            }
+            exit;
+        }
+
+        // Logged in
+        echo '<h3>Access Token</h3>';
+        var_dump($accessToken->getValue());
+
+        // The OAuth 2.0 client handler helps us manage access tokens
+        $oAuth2Client = $this->api->getOAuth2Client();
+
+        // Get the access token metadata from /debug_token
+        $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+        echo '<h3>Metadata</h3>';
+        var_dump($tokenMetadata);
+
+        // Validation (these will throw FacebookSDKException's when they fail)
+        $tokenMetadata->validateAppId($app_id); // Replace {app-id} with your app id
+        // If you know the user ID this access token belongs to, you can validate it here
+        //$tokenMetadata->validateUserId('123');
+        $tokenMetadata->validateExpiration();
+
+        if (! $accessToken->isLongLived()) {
+          // Exchanges a short-lived access token for a long-lived one
+          try {
+            $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+          } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
+            exit;
+          }
+
+          echo '<h3>Long-lived</h3>';
+          var_dump($accessToken->getValue());
+        }
+
+        // $_SESSION['fb_access_token'] = (string) $accessToken;
+        $accessToken = (string) $accessToken;
+        
+
+        $facebook_token = $accessToken;
+        $_SESSION['insta_fb'] = $facebook_token;
+
+        // session()->put('fb_access_token', $accessToken);
+        // echo Session::get('fb_access_token'); die();
+        // header('Location: http://127.0.0.1:3000/fb_connect_app');
+
+        $fb_connect_url = URL::to('/').'/test_instagram';
+
+        return redirect()->away($fb_connect_url);
+    }
+
 }
